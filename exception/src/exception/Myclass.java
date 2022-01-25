ee39ecc14e9a92ff2c42ca04d7f1163bd46f636f
ASHAC <ASHAC@ASHA>
2022-01-25 13:47:12 +0900

    frist commit

---
+++ exception/src/exception/Myclass.java
@@ -0,0 +1,19 @@
+package exception;
+import java.util.*;
+public class Myclass {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		try {
+		int z=x/y;
+		}
+		catch(Exception e) {
+			
+		System.out.println(e);
+		}
+		
+
+	}
+
+}
