759e78273a17cc8e8ed95367e2669e2a95f6bca3
ASHAC <ASHAC@ASHA>
2022-01-25 20:04:11 +0900

    frist commit

---
+++ final/src/Bike1.java
@@ -0,0 +1,19 @@
+
+public class Bike1 {
+	final void run()
+			{
+			System.out.println("running");}
+			}
+			class Honda extends Bike1{
+			void run()
+			{
+			System.out.println("running safely with 100kmph");
+			}
+			public static void main(String args[]){
+			Honda honda= new Honda();
+			honda.run();
+			}
+			
+	}
+
+
---
+++ final/src/Bike12.java
@@ -0,0 +1,22 @@
+
+
+		final class Bike12{
+			void run()
+			{
+			System.out.println("running");}
+			
+			}
+			class Honda1 extends Bike12{
+			void run()
+			{
+			System.out.println("running safely with 100kmph");
+			}
+			public static void main(String args[]){
+			Honda1 honda= new Honda1();
+			honda.run();
+			}
+			}
+
+	
+
+
